50bf8d35b8755a372d9a2937618d1eefffa41fd3
Benjamin Elfen <benjamin@elfen-online.de>
2024-07-02 21:35:26 +0200

    feat: added deployment

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.11.8-slim
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip --no-cache-dir install -r requirements.txt
+
+COPY . /app
+
+# Set default executable
+ENTRYPOINT ["python"]
+
+EXPOSE 8080
+
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import os
+import logging
 from time import sleep
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask.logging import create_logger
+from werkzeug.serving import make_server
+
+# CONFIG
+port = os.environ.get("PORT", 8080)
+host = os.environ.get("HOST", "0.0.0.0")
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = create_logger(app)
 
 
 @app.route("/", methods=["GET"])
@@ -20,7 +31,7 @@ def calculate():
 
     # Print the data to the console
     for key, value in data.items():
-        print(f"{key}: {value}")
+        logger.debug(f"{key}: {value}")
 
     # Calculate the result using model (display waiting screen)
     sleep(2)    # Simulate a long calculation
@@ -35,5 +46,12 @@ def result():
     return render_template("result.html", result=request.args.get("result"))
 
 
+def run_flask_app(app):
+    global server
+    server = make_server(host, port, app)
+    logger.info(f'Starting server on http://{host}:{port}')
+    server.serve_forever()
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    run_flask_app(app)
